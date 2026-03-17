6c8d6f78139445be432587a2218c5a8e92827de9
gulyawar008 <aman.gulyawar@gmail.com>
2026-03-17 07:36:24 +0530

    Added new files

---
+++ Week4__Assignment/BottleFillingMachineHMI/Functions/FunctionJS1.js
@@ -0,0 +1,16 @@
+// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.762.56/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var BottleFillingMachineHMI;
+        (function (BottleFillingMachineHMI) {
+            function FunctionJS1(bConveyorButton) {
+            }
+            BottleFillingMachineHMI.FunctionJS1 = FunctionJS1;
+        })(BottleFillingMachineHMI = Functions.BottleFillingMachineHMI || (Functions.BottleFillingMachineHMI = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('FunctionJS1', 'TcHmi.Functions.BottleFillingMachineHMI', TcHmi.Functions.BottleFillingMachineHMI.FunctionJS1);
+
---
+++ Week4__Assignment/BottleFillingMachineHMI/Functions/FunctionJS2.js
@@ -0,0 +1,15 @@
+// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.762.56/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var BottleFillingMachineHMI;
+        (function (BottleFillingMachineHMI) {
+            function FunctionJS2(par1) {
+            }
+            BottleFillingMachineHMI.FunctionJS2 = FunctionJS2;
+        })(BottleFillingMachineHMI = Functions.BottleFillingMachineHMI || (Functions.BottleFillingMachineHMI = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('FunctionJS2', 'TcHmi.Functions.BottleFillingMachineHMI', TcHmi.Functions.BottleFillingMachineHMI.FunctionJS2);
